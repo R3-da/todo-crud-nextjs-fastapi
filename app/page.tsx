e669cd42a461b6d3139399fe1a4ca488f58c5871
r3-da <haddan.reda.net@gmail.com>
2025-02-17 03:18:25 +0100

    add trash and refresh icons

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import axios from "axios";
+import { FaTrash, FaSyncAlt } from "react-icons/fa";
 
 export default function Home() {
   const [todos, setTodos] = useState<{ id: number; title: string }[]>([]);
@@ -37,13 +38,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24 bg-gradient-to-r from-gray-700 via-gray-900 to-black">
       <div className="fixed top-8 left-0 right-0 z-10 w-full max-w-5xl mx-auto flex items-center justify-center font-mono text-sm lg:flex gap-4">
-        <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="flex w-full justify-center bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing FastApi API&nbsp;
           <Link href="/api/py/helloFastApi">
             <code className="font-mono font-bold">api/index.py</code>
           </Link>
         </p>
-        <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="flex w-full justify-center bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing Next.js API&nbsp;
           <Link href="/api/helloNextJs">
             <code className="font-mono font-bold">app/api/helloNextJs</code>
@@ -52,7 +53,7 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center w-full max-w-md mt-10 mb-8">
-        <h1 className="text-4xl font-bold mb-8">Todo Apps</h1>
+        <h1 className="text-4xl font-bold mb-8">Todo App</h1>
         <input
           type="text"
           value={newTodo}
@@ -69,9 +70,9 @@ export default function Home() {
           </button>
           <button
             onClick={fetchTodos}
-            className="p-2 bg-green-500 text-white rounded"
+            className="w-12 h-12 p-2 bg-green-500 text-white rounded flex items-center justify-center"
           >
-            Refresh
+            <FaSyncAlt />
           </button>
         </div>
       </div>
@@ -87,7 +88,7 @@ export default function Home() {
                 onClick={() => removeTodo(todo.id)}
                 className="text-red-500"
               >
-                Remove
+                <FaTrash />
               </button>
             </li>
           ))}
